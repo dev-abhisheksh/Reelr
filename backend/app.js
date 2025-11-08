f02a24d65eb0e8a9385e69da8fa2ec762856d38c
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-11-08 22:10:49 +0530

    update CORS origin to use production URL

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://reelr.vercel.app/",
     credentials: true  
 }));
 
