86199e78240c963a06303bf19846ba5e2f518ff7
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-12-18 23:32:52 +0530

    feat: add rate limiter middleware for API request control

---
+++ backend/middlewares/rateLimiter.js
@@ -0,0 +1,30 @@
+import Redis from "ioredis";
+import { client } from "../utils/redis-client.js";
+
+
+const rateLimiter = ({ keyPrefix, limit, windowSec }) => {
+    return async (req, res, next) => {
+        try {
+            const identifier = req.user?._id ? `user:${req.user._id}` : `ip:${req.ip}`
+
+            const rediskey = `rate:${keyPrefix}:${identifier}`
+
+            const [[, count]] = await client
+                .multi()
+                .incr(rediskey)
+                .expire(rediskey, windowSec)
+                .exec();
+                
+            if (count > limit) {
+                return res.status(429).json({ message: "Too many requests. Please try again later." })
+            }
+
+            next()
+        } catch (error) {
+            console.error("Rate limiter error", error)
+            next()
+        }
+    }
+}
+
+export default rateLimiter
--- backend/routes/reel.routes.js
+++ backend/routes/reel.routes.js
@@ -3,12 +3,13 @@ import verifyJWT from "../middlewares/auth.middleware.js";
 import allowRoles from "../middlewares/role.middleware.js";
 import { upload } from "../config/multer.js";
 import { deleteReel, getAllReels, getReelById, getReelsByUser, updateReel, uploadReel, getTrendingReels, incrementViews, getTotalViewsOfCreator, likeUnlikeReel } from "../controllers/reel.controller.js";
+import rateLimiter from "../middlewares/rateLimiter.js";
 
 const router = express.Router();
 
 router.post("/upload", verifyJWT, upload.single("video"), uploadReel)
 
-router.get("/all", getAllReels)
+router.get("/all", rateLimiter({ keyPrefix: "allReels", limit: 2, windowSec: 60 }), getAllReels)
 router.delete("/delete/:id", verifyJWT, deleteReel)
 router.get("/single-reel/:id", verifyJWT, getReelById)
 router.patch("/update/:id", verifyJWT, updateReel)
