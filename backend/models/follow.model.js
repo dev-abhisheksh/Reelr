7abdfb61831820542ab3b1ad548f53305ff1641c
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-04-23 14:23:02 +0530

    feat: Add Follow model to manage user follow relationships

---
+++ backend/models/follow.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const followSchema = new mongoose.Schema({
+    follower: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    following: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true });
+
+followSchema.index({ follower: 1, following: 1 }, {unique: true})
+
+export const Follow = mongoose.model("Follow", followSchema)
