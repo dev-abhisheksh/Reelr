70579bce227293391a7ba75f2bc7494cbd5341d5
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-04-23 16:36:42 +0530

    feat: Add getFeed functionality to retrieve posts from followed users

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import { Follow } from "../models/follow.model.js";
 import { Post } from "../models/post.model.js";
 import { uploadImage } from "../utils/uploadImage.js";
 
@@ -37,11 +38,11 @@ const createPost = async (req, res) => {
     }
 }
 
-const getPostsByUser = async(req,res)=>{
+const getPostsByUser = async (req, res) => {
     try {
         const userId = req.user._id;
 
-        const posts = await Post.find({userId})
+        const posts = await Post.find({ userId })
         return res.status(200).json({
             message: "Fetched posts of user",
             count: posts.length,
@@ -49,11 +50,43 @@ const getPostsByUser = async(req,res)=>{
         })
     } catch (error) {
         console.error("Failed to fetch posts", error)
-        return res.status(500).json({message: "Failed to fetch psosts"})
+        return res.status(500).json({ message: "Failed to fetch psosts" })
+    }
+}
+
+const getFeed = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const followingDocs = await Follow.find({
+            follower: userId,
+            status: "accepted"
+        }).select("following")
+
+        const followingIds = followingDocs.map(doc => doc.following)
+
+        followingIds.push(userId)
+
+        const posts = await Post.find({
+            userId: { $in: followingIds },
+            isDeleted: false
+        })
+            .sort({ createdAt: -1 })
+            .limit(200)
+
+        return res.status(200).json({
+            count: posts.length,
+            posts
+        })
+
+    } catch (error) {
+        console.error("Feed error:", error);
+        return res.status(500).json({ message: "Failed to fetch feed" });
     }
 }
 
 export {
     createPost,
-    getPostsByUser
+    getPostsByUser,
+    getFeed
 }
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import verifyJWT from "../middlewares/auth.middleware.js"
 import { upload } from "../config/multer.js"
-import { createPost, getPostsByUser } from "../controllers/post.controller.js"
+import { createPost, getFeed, getPostsByUser } from "../controllers/post.controller.js"
 
 const router = express.Router()
 
 router.post("/create", verifyJWT, upload.single("image"), createPost)
 router.get("/", verifyJWT, getPostsByUser)
+router.get("/feed", verifyJWT, getFeed)
 
 export default router;  
