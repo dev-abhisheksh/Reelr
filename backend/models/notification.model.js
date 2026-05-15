50ebe52c00403996628712c4f15b04fa3f8b31eb
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-05-15 20:14:43 +0530

    feat: Add Notification model for managing user notifications with schema definition

---
+++ backend/models/notification.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const notificatonSchema = new mongoose.Schema({
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        required: true
+    },
+
+    type: {
+        type: String,
+        enum: ["post", "follow-request", "like", "comment"],
+        required: true
+    },
+
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    isRead: {
+        type: Boolean,
+        default: false
+    }
+
+}, { timestamps: true })
+
+notificatonSchema.index({ reciever: 1, createdAt: -1 })
+
+export const Notification = mongoose.model("Notification", notificatonSchema)
