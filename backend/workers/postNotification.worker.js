2ac65bd4aac23673536c8b8a2ab34e13b84b91eb
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-05-16 22:23:36 +0530

    feat: Enhance postNotification worker to emit real-time notifications to online users

--- backend/workers/postNotification.worker.js
+++ backend/workers/postNotification.worker.js
@@ -3,6 +3,7 @@ import { Follow } from "../models/follow.model.js";
 import { Notification } from "../models/notification.model.js";
 import { client } from "../utils/redis-client.js";
 import connectDB from "../config/db.js";
+import { getIO, onlineUser } from "../socket.js";
 
 connectDB()
 
@@ -23,6 +24,15 @@ const worker = new Worker("post-notification", async (job) => {
         }))
 
         await Notification.insertMany(notifications)
+
+        const io = getIO();
+
+        Notification.forEach((notification) => {
+            const recieverSocketId = onlineUser.get(notification.receiver.toString());
+            if (recieverSocketId) {
+                io.on(recieverSocketId).emit("new-notification", notification)
+            }
+        })
 
         console.log("Notification created")
     } catch (error) {
