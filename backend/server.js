0ddadfbcfdfd763dc4a2a7a8fed0b6e68f1c975d
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-05-16 22:20:48 +0530

    feat: Refactor server setup to use http server with socket.io integration

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,15 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import { Server } from "socket.io";
+import http from "http"
+import { initSocket } from "./socket.js";
 
-
-// app.use("/", (req, res) => {
-//   res.send("Welcome, To the journey to be a great mern engineer");
-// });
+const server = http.createServer(app)
+initSocket(server)
 
 connectDB()
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port:${process.env.PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port: ${process.env.PORT}`);
 });
