fa6287d7266a1f101df89683c5e740eff3421727
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-05-19 09:58:04 +0530

    feat: enhance SearchPage component with input state management and clear functionality

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 const SearchPage = () => {
+
+  const [search, setSearch] = useState("");
+
+  const clearInputField = () => {
+    setSearch("");
+  }
+
   return (
     <div className='flex justify-center h-screen w-full bg-[#0c1115]'>
-      <div className='flex px-4  items-center justify-between  mt-5 h-10 w-[95%] bg-[#24292f] rounded-3xl'>
-        <input className='text-white broder-0 outline-0' type="text" placeholder='Search' />
-        <div className='rounded-full bg-[#e9e9e9] w-5 h-5 flex items-center justify-center'>
-          <span className='text-s leading-none'>×</span>
-        </div>
+      <div className='flex px-4 items-center justify-between mt-5 h-10 w-[95%] bg-[#24292f] rounded-3xl'>
+
+        <input
+          className='text-white bg-transparent border-0 outline-0 w-full'
+          type="text"
+          placeholder='Search'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        {
+          search && (
+            <IoIosCloseCircleOutline
+              onClick={clearInputField}
+              size={20}
+              className='text-white'
+            />
+          )
+        }
+
       </div>
     </div>
   )
