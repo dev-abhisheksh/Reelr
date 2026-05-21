deded4c6537ec44ca9237cc23e8df3279eb1bf90
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-05-22 00:26:51 +0530

    feat: add isFollowing endpoint to follow routes

--- backend/routes/follow.route.js
+++ backend/routes/follow.route.js
@@ -1,13 +1,15 @@
 import express from "express"
 import verifyJWT from "../middlewares/auth.middleware.js";
-import { acceptFollowRequest, followUser, getFollowRequests, unfollowUser, userStats } from "../controllers/follow.controller.js";
+import { acceptFollowRequest, followUser, getFollowRequests, isFollowing, unfollowUser, userStats } from "../controllers/follow.controller.js";
 
 const router = express.Router();
 
 router.get("/requests", verifyJWT, getFollowRequests)
-router.post("/:followUserId", verifyJWT, followUser)
 router.patch("/accept/:followRequestId", verifyJWT, acceptFollowRequest)
 router.patch("/unfollow/:unfollowUserId", verifyJWT, unfollowUser)
 router.get("/stats", verifyJWT, userStats)
+router.get("/is-following/:userId", verifyJWT, isFollowing)
+
+router.post("/:followUserId", verifyJWT, followUser)
 
 export default router
