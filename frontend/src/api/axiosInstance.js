2c3feb51cf6a3ba3461f59ccf253a0c3b4bbecfb
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-23 21:12:46 +0530

    feat: remove token interceptor and simplify axios instance; add addComment function

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,17 +1,6 @@
 import axios from "axios";
 
-const getToken = () => localStorage.getItem("accessToken");
-
 export const API = axios.create({
     baseURL: "http://localhost:8000",
+    withCredentials: true,
 });
-// https://reelr.onrender.com
-// ✅ Attach Bearer token to all requests automatically
-API.interceptors.request.use((config) => {
-    const token = getToken();
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
