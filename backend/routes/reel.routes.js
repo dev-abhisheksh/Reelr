b6eedf7c5e8c92bddc70df6811e85403017b1c03
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-27 19:43:38 +0530

    feat: remove optionalAuth middleware and enforce JWT verification for all reels retrieval

--- backend/routes/reel.routes.js
+++ backend/routes/reel.routes.js
@@ -1,6 +1,5 @@
 import express from "express"
 import verifyJWT from "../middlewares/auth.middleware.js";
-import optionalJWT from "../middlewares/optionalAuth.middleware.js";
 import allowRoles from "../middlewares/role.middleware.js";
 import { deleteReel, getAllReels, getReelById, getReelsByUser, updateReel, uploadReel, getTrendingReels, incrementViews, getTotalViewsOfCreator } from "../controllers/reel.controller.js";
 import rateLimiter from "../middlewares/rateLimiter.js";
@@ -10,7 +9,7 @@ const router = express.Router();
 
 router.post("/upload", verifyJWT, uploadReelMiddleware, uploadReel)
 
-router.get("/all", optionalJWT, rateLimiter({ keyPrefix: "allReels", limit: 30, windowSec: 60 }), getAllReels)
+router.get("/all", verifyJWT, rateLimiter({ keyPrefix: "allReels", limit: 30, windowSec: 60 }), getAllReels)
 router.delete("/delete/:id", verifyJWT, deleteReel)
 router.get("/single-reel/:id", verifyJWT, getReelById)
 router.patch("/update/:id", verifyJWT, updateReel)
