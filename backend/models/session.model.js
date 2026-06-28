2d8621fc287e49cc17bcfcf5e49a406750475b7f
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-28 16:19:55 +0530

    feat: add session model with schema for user sessions, including refresh token and metadata

---
+++ backend/models/session.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const sessionSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User is required"]
+    },
+
+    refreshTokenHash: {
+        type: String,
+        required: [true, "RefreshToken hash is required"]
+    },
+
+    ip: {
+        type: String,
+        required: [true, "IP Address is required"]
+    },
+
+    userAgent: {
+        type: String,
+        required: [true, "User Agent is required"]
+    },
+
+    revoked: {
+        type: Boolean,
+        defaultfalse
+    }
+
+}, { timestamps: true })
+
+export const Session = mongoose.model("Session", sessionSchema)
