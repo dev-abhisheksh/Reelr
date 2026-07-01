ce3684611716507c8ed71b79b2cdac1c92757347
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-07-01 21:29:47 +0530

    feat: add StatusView model to track user views on statuses

---
+++ backend/modules/statusViews/statusView.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const statusViewSchema = new mongoose.Schema({
+    statusId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Status",
+        required: true
+    },
+
+    viewerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    viewdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 86400
+    }
+}, { timestamps: false })
+
+statusViewsSchema.index({ statusId: 1, viewerId: 1 }, { unique: true })
+
+export const StatusView = mongoose.model("StatusView", statusViewSchema)
